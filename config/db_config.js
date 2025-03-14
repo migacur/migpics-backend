5da354d5d8a5b29adb22d74a5347e262fbdb54e2
migacur <78935454+migacur@users.noreply.github.com>
2025-03-14 17:52:30 -0400

    cambio config bbdd

--- config/db_config.js
+++ config/db_config.js
@@ -1,19 +1,22 @@
 const mysql = require('mysql2');
 
-const db_config = mysql.createConnection({
-    host:process.env.HOST,
-    user:process.env.USER,
-    password:process.env.PASSWORD,
-    database:process.env.DATABASE
+const db_config = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db_config.connect(function(err) {
+db_config.getConnection((err, connection) => {
   if (err) {
-    console.error('Error conectando a: ' + err.stack);
+    console.error('Error al obtener conexión del pool:', err);
     return;
   }
- 
-  console.log('Conectado a BBDD con ID ' + db_config.threadId);
+  console.log('Conexión exitosa a la BBDD. ID de conexión:', connection.threadId);
+  connection.release(); // Libera la conexión al pool
 });
 
-module.exports = db_config
+module.exports = db_config;
