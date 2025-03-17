9d146c7f33181f20098945dfc042739c013a9cf1
migacur <78935454+migacur@users.noreply.github.com>
2025-03-17 11:40:19 -0400

    cambio download controller

--- controllers/downloads.js
+++ controllers/downloads.js
@@ -1,35 +1,35 @@
 const { request, response } = require("express");
-const fs = require('fs');
 const axios = require('axios');
-const os = require('os');
-const path = require('path');
 const actualizarDescargas = require("../helpers/updateDownloads");
 const { v4: uuidv4 } = require('uuid');
 
-const descargarImagen = async (req=request,res=response) => {
+const descargarImagen = async (req = request, res = response) => {
+  const { postId } = req.params;
+  const randomUrl = uuidv4();
 
-    const { postId } = req.params;
-    const randomUrl = uuidv4();
+  try {
+      const imageUrl = req.body.url;
+      const ext = imageUrl.split('.').pop();
+      
+      // Obtener la imagen directamente
+      const imageResponse = await axios.get(imageUrl, { 
+          responseType: 'stream' 
+      });
 
-    try {
-        const imageUrl = req.body.url; // Obtén la URL de la imagen desde la solicitud del cliente
-        const ext = imageUrl.split('.').pop();
-        const imageResponse  = await axios.get(imageUrl, { responseType: 'stream' }); // Realiza una solicitud HTTP para obtener la imagen
-    
-        const desktopPath = path.join(os.homedir(), 'Desktop');
-        const imagePath = path.join(desktopPath, `${randomUrl}.${ext}`);// Define la ruta donde deseas guardar la imagen
-    
-        imageResponse.data.pipe(fs.createWriteStream(imagePath)); // Guarda la imagen en el disco
-    
-        actualizarDescargas(postId)
-        return res.status(200).json({ msg: 'La imagen se ha descargado exitosamente' });
-      } catch (error) {
-        console.error(error);
-        return res.status(500).json({ msg: 'Error al descargar la imagen' });
-      } 
+      // Configurar headers para forzar la descarga
+      res.setHeader('Content-Disposition', `attachment; filename="${randomUrl}.${ext}"`);
+      res.setHeader('Content-Type', imageResponse.headers['content-type']);
 
+      // Pipe de la imagen desde la respuesta de axios al cliente
+      imageResponse.data.pipe(res);
 
-}
+      actualizarDescargas(postId);
+
+  } catch (error) {
+      console.error(error);
+      return res.status(500).json({ msg: 'Error al descargar la imagen' });
+  }
+};
 
 
 module.exports = {
