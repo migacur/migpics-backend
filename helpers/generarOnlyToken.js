4e3fd659a7fd3853e8d8467b0e6d8ca90ebe5b88
migacur <78935454+migacur@users.noreply.github.com>
2025-05-21 19:40:54 -0400

    cambio fn generarOnlyToken

--- helpers/generarOnlyToken.js
+++ helpers/generarOnlyToken.js
@@ -1,21 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 const generarOnlyToken = (user) => {
-   console.log("--- TOKEN ONLY ---")
-   console.log(user)
-   console.log("--- TOKEN ONLY ---")
-   const { username, user_id } = user;
+  console.log("--- TOKEN ONLY ---");
+  console.log(user);
+  console.log("--- TOKEN ONLY ---");
+  const { username, id } = user; 
 
-   const payload = {
-    username:username,
-    id:user_id
-   }
-  
-  return jwt.sign(
-    payload,
-    process.env.TOKEN_KEY,
-    { expiresIn: "1h" }
-  );
+  const payload = {
+    username,
+    id,
+  };
+
+  return jwt.sign(payload, process.env.TOKEN_KEY, { expiresIn: "1h" });
 };
 
 module.exports = generarOnlyToken;
